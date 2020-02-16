e9e4ab946f47517f71f091ebf6920b77592c31ef
alex <frol1414@gmail.com>
2020-02-16 13:10:49 +0300

    add history page and some fix

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -25,6 +25,8 @@
 <script>
 import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
+import messages from '@/utils/messages'
+
 export default {
   name: 'main-layout',
   data: () => ({
@@ -43,5 +45,16 @@ export default {
   components: {
     Navbar, Sidebar,
   },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+
+  watch: {
+    error(fbError) {
+      this.$error(messages[fbError.code] || 'Something went wrong')
+    }
+  }
 };
 </script>
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
     'logout': 'You logout from the system',
+    'login': 'For start, enter to the system',
     'auth/user-not-found': 'User not found',
     'auth/wrong-password': 'You wrote wrong password',
     'auth/email-already-in-use': 'Such email already exists'
